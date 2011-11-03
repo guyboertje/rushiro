cd6bb82634bc66541592d8386e23bd63ce6f8bd2
Guy Boertje <guyboertje@gmail.com>
2011-11-03 08:09:41 +0000

    add AllowBasedControl

--- lib/rushiro.rb
+++ lib/rushiro.rb
@@ -1,4 +1,4 @@
-%w[permission permissions access_levels access_control_hash deny_based_control].each do |file|
+%w[permission permissions access_levels access_control_hash deny_based_control allow_based_control].each do |file|
   require "rushiro/#{file}"
 end
 
---
+++ lib/rushiro/allow_based_control.rb
@@ -0,0 +1,9 @@
+module Rushiro
+  class AllowBasedControl < AccessControlHash
+    def permitted?(perm)
+      return false if !@dirty && @original.empty?
+      
+      @allows.permitted?(perm)
+    end
+  end
+end
